6154bba6ecb4fd1e9f02abf76f6dcdfd58002b37
Keith <harderkeith@gmail.com>
2017-01-17 21:53:34 -0600

    Simple Socket program updates Send/Receive

--- SocketClient/Form1.Designer.cs
+++ SocketClient/Form1.Designer.cs
@@ -33,63 +33,87 @@
             this.lblIP = new System.Windows.Forms.Label();
             this.lblPort = new System.Windows.Forms.Label();
             this.btnConnect = new System.Windows.Forms.Button();
+            this.senderTextBox = new System.Windows.Forms.TextBox();
+            this.recTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtIP
             // 
-            this.txtIP.Location = new System.Drawing.Point(27, 12);
+            this.txtIP.Location = new System.Drawing.Point(20, 10);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(144, 22);
+            this.txtIP.Size = new System.Drawing.Size(109, 20);
             this.txtIP.TabIndex = 0;
             this.txtIP.Text = "192.168.0.1";
             // 
             // txtPort
             // 
-            this.txtPort.Location = new System.Drawing.Point(201, 12);
+            this.txtPort.Location = new System.Drawing.Point(151, 10);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(88, 22);
+            this.txtPort.Size = new System.Drawing.Size(67, 20);
             this.txtPort.TabIndex = 1;
             this.txtPort.Text = "26969";
             // 
             // lblIP
             // 
             this.lblIP.AutoSize = true;
-            this.lblIP.Location = new System.Drawing.Point(61, 37);
+            this.lblIP.Location = new System.Drawing.Point(46, 30);
+            this.lblIP.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblIP.Name = "lblIP";
             this.lblIP.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.lblIP.Size = new System.Drawing.Size(76, 17);
+            this.lblIP.Size = new System.Drawing.Size(58, 13);
             this.lblIP.TabIndex = 2;
             this.lblIP.Text = "IP Address";
             // 
             // lblPort
             // 
             this.lblPort.AutoSize = true;
-            this.lblPort.Location = new System.Drawing.Point(228, 37);
+            this.lblPort.Location = new System.Drawing.Point(171, 30);
+            this.lblPort.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPort.Name = "lblPort";
-            this.lblPort.Size = new System.Drawing.Size(34, 17);
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
             this.lblPort.TabIndex = 3;
             this.lblPort.Text = "Port";
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(104, 96);
+            this.btnConnect.Location = new System.Drawing.Point(80, 63);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(105, 31);
+            this.btnConnect.Size = new System.Drawing.Size(79, 25);
             this.btnConnect.TabIndex = 4;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
             this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
+            // senderTextBox
+            // 
+            this.senderTextBox.Location = new System.Drawing.Point(20, 93);
+            this.senderTextBox.Name = "senderTextBox";
+            this.senderTextBox.Size = new System.Drawing.Size(198, 20);
+            this.senderTextBox.TabIndex = 5;
+            // 
+            // recTextBox
+            // 
+            this.recTextBox.Location = new System.Drawing.Point(20, 119);
+            this.recTextBox.Name = "recTextBox";
+            this.recTextBox.Size = new System.Drawing.Size(198, 20);
+            this.recTextBox.TabIndex = 6;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(333, 266);
+            this.ClientSize = new System.Drawing.Size(250, 216);
+            this.Controls.Add(this.recTextBox);
+            this.Controls.Add(this.senderTextBox);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.lblPort);
             this.Controls.Add(this.lblIP);
             this.Controls.Add(this.txtPort);
             this.Controls.Add(this.txtIP);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -104,6 +128,8 @@
         private System.Windows.Forms.Label lblIP;
         private System.Windows.Forms.Label lblPort;
         private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox senderTextBox;
+        private System.Windows.Forms.TextBox recTextBox;
     }
 }
 
--- SocketClient/Form1.cs
+++ SocketClient/Form1.cs
@@ -27,7 +27,20 @@ namespace SocketClient
                 int port = Convert.ToInt32(txtPort.Text);
 
                 TcpClient client = new TcpClient(serverIP, port);
+
+                Byte[] data = Encoding.ASCII.GetBytes(senderTextBox.Text);
                 
+
+                NetworkStream netStream = client.GetStream();
+                netStream.Write(data, 0, data.Length);
+
+                data = new Byte[2048];
+
+                int bytesToRec = netStream.Read(data,0,data.Length);
+                string msg = Encoding.ASCII.GetString(data, 0, bytesToRec);
+
+                recTextBox.Text = msg;
+
             }
             catch (Exception ex)
             {
