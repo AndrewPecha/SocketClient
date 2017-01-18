84e93230c0c23991cabd3db42eb4ac07bee90b60
Andrew Pecha <andrew.pecha@gmail.com>
2017-01-18 17:54:04 -0600

    lowercase byte plz

--- SocketClient/Form1.cs
+++ SocketClient/Form1.cs
@@ -28,13 +28,12 @@ namespace SocketClient
 
                 TcpClient client = new TcpClient(serverIP, port);
 
-                Byte[] data = Encoding.ASCII.GetBytes(senderTextBox.Text);
-                
+                byte[] data = Encoding.ASCII.GetBytes(senderTextBox.Text);                
 
                 NetworkStream netStream = client.GetStream();
                 netStream.Write(data, 0, data.Length);
 
-                data = new Byte[2048];
+                data = new byte[2048];
 
                 int bytesToRec = netStream.Read(data,0,data.Length);
                 string msg = Encoding.ASCII.GetString(data, 0, bytesToRec);
